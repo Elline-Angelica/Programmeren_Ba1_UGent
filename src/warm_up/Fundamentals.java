adbdaa3098494a80db7c6593df17b209bdac5b56
emedrano <Elline.Medrano@UGent.be>
2022-01-23 22:46:49 +0100

    Fundamentals excercises

---
+++ src/warm_up/Fundamentals.java
@@ -0,0 +1,30 @@
+package warm_up;
+
+public class Fundamentals {
+
+    /*
+    * 1. b
+    * 2. c
+    * 3. d
+    * 4. javac HelloWorldApp.java | java HelloWorldApp
+    * 5. public static void main(String[] args){}
+    * 6. a
+    * 7. b
+    * 8. a,c,f
+    * 9. c
+    * 10. c
+    * 11. a
+    * 12. int a; int b; | int x = 10; char ch = 'y'; | double payRate = 12.50; | boolean ans = true;
+    * 13. 3 | 5 | 1 | -2 | 4.25 | -2.0 | 12 | 16
+    * 14.
+    * 15. 22
+    * 16. x *= 2 | x += y-2 | sum += sum | y /= (x+5)
+    * 17.
+    * 18. class FixProgram {
+        public static void main(String[] args) {
+        System.out.println('Hello World!');
+             }
+        }
+    * */
+
+}
---
+++ src/warm_up/Input_Output.java
@@ -0,0 +1,45 @@
+package warm_up;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Input_Output {
+    public static void main(String[] args) {
+        q3();
+    }
+
+    public static void q1(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer as radius of a circle");
+        int radius = scanner.nextInt();
+        double circum = (2*radius*Math.PI);
+        double area = (radius*radius*Math.PI);
+        System.out.println("Circumference: " + circum + "\nArea: " + area);
+    }
+
+    public static void q2(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter length side a");
+        double a = scanner.nextDouble();
+        System.out.println("Enter length side b");
+        double b = scanner.nextDouble();
+        System.out.println("Enter length side c");
+        double c = scanner.nextDouble();
+        double s = (a + b + c)/2;
+        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        System.out.println("The area of the triangle is = " + area);
+    }
+
+    public static void q3(){
+         String input;
+         double c;
+         double f;
+
+         input = JOptionPane.showInputDialog("Enter temperature in Celcius");
+         c = Double.parseDouble(input);
+         f = 1.8 * c + 32;
+
+         JOptionPane.showMessageDialog(null,"Temperature in Fahrenheit is " + f);
+    }
+
+}
